474f76aab6df47f242fb41e97416cda05799bf1d
HugoRod <hugsrodriguez8@gmail.com>
2021-01-28 11:01:10 -0600

    Se creo el repositorio de mi-primera-app

---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-personas',//Nombre del comopente dentro de HTML
+  templateUrl: './personas.component.html'//Plantilla HTML que vamos a utilizar
+})
+export class PersonasComponent{
+
+}
